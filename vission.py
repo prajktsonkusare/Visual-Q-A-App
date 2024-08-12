4a0650bd68f1a4a3c33b834483ce64cf3471f205
Prajkt Sonkusare <137401330+prajktsonkusare@users.noreply.github.com>
2024-08-12 17:25:23 +0530

    vission.py

--- vission.py
+++ vission.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 from PIL import Image
 
 # Configure the Google Generative AI API
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv('AIzaSyBfv3Jx30PKhyBvet479WSGuwWVkq-_66Y'))
 
 # Initialize the GenerativeModel
 model = genai.GenerativeModel("gemini-1.5-pro")
